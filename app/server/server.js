2acc7c0bfe1a0d868cc31a74f1bce6b149d82afd
Mike Jed <jedmike@gmail.com>
2020-01-17 22:06:41 -0500

    redid directory structure, simplified

---
+++ app/server/server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+
+const clientPath = '..//client/';
+console.log(clientPath);
+console.log('Serving static from $ {clientPath}');
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const io = socket(server);
+
+io.on('connection', (sock) => {
+    sock.emit('message', 'u r conneced ');
+});
+
+server.on("error", (err) => {
+    console.error("Server error:", err);
+});
+
+server.listen(3000, () => {
+    console.log("Battleship Started on 3000");
+});
